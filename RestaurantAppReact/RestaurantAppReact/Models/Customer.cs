c73dfb52e11a5d04900f792626b035ff9afe6e01
Dominika Barrett <barrettdomi@gmail.com>
2021-06-28 09:49:42 +0100

    added migration

---
+++ RestaurantAppReact/RestaurantAppReact/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantAppReact.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        
+       
+        public string CustomerName { get; set; }
+        
+    }
+}
---
+++ RestaurantAppReact/RestaurantAppReact/Models/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAppReact.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public long OrderDetailsId { get; set; }
+        public long OrderMasterId { get; set; }
+       
+        public int FoodItemId { get; set; }
+        public FoodItem FoodItem { get; set; }
+        public decimal FoodItemPrice { get; set; }
+        public int Quantity { get; set; }
+        
+    }
+}
